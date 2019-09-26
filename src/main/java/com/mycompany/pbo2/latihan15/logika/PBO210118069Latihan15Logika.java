babbecb8ad9919cc4132a302576fc2a3a45b870f
Muhamad Rizqi Ramdani <55569266+MuhamadRizqiRamdani@users.noreply.github.com>
2019-09-26 15:01:26 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/pbo2/latihan15/logika/PBO210118069Latihan15Logika.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbo2.latihan15.logika;
+
+/**
+ *
+ * @author
+ * NAMA     : Muhamad Rizqi Ramdani
+ * KELAS    : PBO2
+ * NIM      : 10118069
+ * Deskripsi program : Program ini berisi program untuk menampilkan hasil Logika
+ */
+public class PBO210118069Latihan15Logika {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b) ) ;
+    }
+    
+}
